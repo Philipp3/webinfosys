db9ab724e424afb10e8d97a8a63d32ce363201bc
Philipp Peringer <phirxos@gmail.com>
2015-03-25 15:12:41 +0100

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 namespace grp12;
 include("php/templates/template.php");
 include("php/menu.php");
-
+ 
 $template = new template\Template("php/templates/index.phtml");
 
 $template -> title = "Startseite";

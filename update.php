dd460b1d2fe38dc4735e220e4e2e430751c0be93
Philipp Peringer <phirxos@gmail.com>
2015-03-25 15:02:27 +0100

    changed updates to work with unclean /var/www/html

--- update.php
+++ update.php
@@ -1,6 +1,7 @@
 <?php
   header("Content-type: text/plain");
-  echo(trim(shell_exec("git pull")));
+  echo(trim(shell_exec("git reset --hard HEAD 2>&1")));
+  echo(trim(shell_exec("git pull 2>&1")));
   echo("\n\nStatus:\n");
-  echo(trim(shell_exec("git status")));
+  echo(trim(shell_exec("git status 2>&1")));
 ?>

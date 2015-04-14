fe71a36452e535511e99f7856a855c6e5da6fce4
Philipp Peringer <phirxos@gmail.com>
2015-04-14 13:17:40 +0200

    reworked login functionality

--- index.php
+++ index.php
@@ -1,59 +1,113 @@
 <?php
+
 namespace grp12;
 
 const SERVERPATH = "/var/www/html/";
 
-require_once SERVERPATH."php/templates/template.php";
-require_once SERVERPATH."php/user.php";
+require_once SERVERPATH . "php/templates/template.php";
+require_once SERVERPATH . "php/user.php";
 
-$maintemplate = new template\Template(SERVERPATH."php/templates/base.phtml");
+$maintemplate = new template\Template ( SERVERPATH . "php/templates/base.phtml" );
 
-$usermgr = user\userMgr::getInstance();
-
-
-$menuentries = array(
-		"/" => "Startseite",
-		"/team/" => "Über das Team",
-		"/img/" => "Beispielseite",
-		"/myspot/" => "MySpot");
-if($usermgr->loggedin)
-	$menuentries["/login.php?action=logout"] = $usermgr->username." abmelden";
-else
-	$menuentries["/login.php"] = "Anmelden";
-
-$maintemplate->menuentries = $menuentries;
-
-if($usermgr->loggedin)
-	$maintemplate->sidebarcontent = "Angemeldet als ".$usermgr->username;
-else
-	$maintemplate->sidebarcontent = "Nicht angemeldet.";
+$usermgr = user\userMgr::getInstance ();
 
 $site = "index";
-if(isset($_GET["site"])) $site = $_GET["site"];
+if (isset ( $_GET ["site"] ))
+	$site = $_GET ["site"];
 
-switch($site) {
-	case "index":
+$sidetemplate = null;
+
+switch ($site) {
+	case "index" :
 		$maintemplate->pagetitle = "Startseite";
-		$sidetemplate = new template\Template(SERVERPATH."php/templates/index.phtml");
-		$maintemplate->content =  $sidetemplate->outString();
+		$sidetemplate = new template\Template ( SERVERPATH . "php/templates/index.phtml" );
 		break;
-	case "img":
+	case "img" :
 		$maintemplate->pagetitle = "Beispielseite für Fließtext";
-		$sidetemplate = new template\Template(SERVERPATH."php/templates/img.phtml");
-		$maintemplate->content = $sidetemplate->outString();
+		$sidetemplate = new template\Template ( SERVERPATH . "php/templates/img.phtml" );
 		break;
-	case "team":
+	case "team" :
 		$maintemplate->pagetitle = "Über das Team";
-		$sidetemplate = new template\Template(SERVERPATH."php/templates/team.phtml");
-		$maintemplate->content = $sidetemplate->outString();
+		$sidetemplate = new template\Template ( SERVERPATH . "php/templates/team.phtml" );
 		break;
-	default:
+	case "login" :
+		$sidetemplate = new template\Template ( SERVERPATH . "php/templates/login.phtml" );
+		$sidetemplate->referer = $_SERVER ["HTTP_REFERER"];
+		
+		if (isset ( $_POST ["username"] )) {
+			$username = $_POST ["username"];
+			$password = $_POST ["password"];
+			$referer = $_POST ["referer"];
+			$sidetemplate->referer = $referer;
+			if (! $usermgr->loggedin) {
+				$return = $usermgr->login ( $username, $password );
+				switch ($return) {
+					case user\userMgr::EXIT_SUCCESS :
+						$maintemplate->pagetitle = "Anmeldung erfolgreich";
+						$sidetemplate->loggedin = true;
+						$sidetemplate->infomsg = "infLoggedIn";
+						break;
+					case user\userMgr::ERROR_NONEXISTING_USER :
+					case user\userMgr::ERROR_WRONG_PASSWORD :
+						$maintemplate->pagetitle = "Fehler";
+						$sidetemplate->loggedin = false;
+						$sidetemplate->errmsg = "errUserPwd";
+						break;
+					case user\userMgr::ERROR_ESTABLISHING_DB_CONNECTION :
+						$maintemplate->pagetitle = "Fehler";
+						$sidetemplate->loggedin = false;
+						$sidetemplate->errmsg = "errDbCon";
+						break;
+				}
+			}
+		} else {
+			
+			if ($usermgr->loggedin ()) {
+				$sidetemplate->loggedin = true;
+				if ($_GET ["action"] == "logout") {
+					$usermgr->logout ();
+					$maintemplate->pagetitle = "Abmeldung erfolgreich";
+					$sidetemplate->infomsg = "infLoggedOut";
+				} else {
+					$sidetemplate->pagetitle = "Angemeldet";
+					$sidetemplate->infomsg = "infLoggedIn";
+				}
+			} else {
+				$maintemplate->pagetitle = "Anmelden";
+				$sidetemplate->loggedin = false;
+				if ($_GET ["action"] == "logout") {
+					$sidetemplate->infotype = "infoerr";
+					$sidetemplate->infomsg = "Sie sind bereits abgemeldet.";
+				}
+			}
+		}
+		break;
+	default :
 		$maintemplate->pagetitle = "Seite nicht gefunden";
-		$sidetemplate = new template\Template(SERVERPATH."php/templates/404.phtml");
-		$maintemplate->content = $sidetemplate->outString();
+		$sidetemplate = new template\Template ( SERVERPATH . "php/templates/404.phtml" );
 		break;
 }
 
-$maintemplate -> out();
+$menuentries = array (
+		"/" => "Startseite",
+		"/team/" => "Über das Team",
+		"/img/" => "Beispielseite",
+		"/myspot/" => "MySpot"
+);
+if ($usermgr->loggedin)
+	$menuentries ["/login.php?action=logout"] = $usermgr->username . " abmelden";
+else
+	$menuentries ["/login.php"] = "Anmelden";
+
+$maintemplate->menuentries = $menuentries;
+
+if ($usermgr->loggedin)
+	$maintemplate->sidebarcontent = "Angemeldet als " . $usermgr->username;
+else
+	$maintemplate->sidebarcontent = "Nicht angemeldet.";
+
+$maintemplate->content = $sidetemplate->outString ();
+
+$maintemplate->out ();
 
 ?>

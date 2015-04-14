e49504813257c3e5ac6b3c0e9c3ddf94237e3f7f
Philipp Peringer <phirxos@gmail.com>
2015-04-14 12:19:08 +0200

    new branch

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 header("Content-type: text/plain");
 echo("Git-update script:\n\n");
 echo("Resetting possible changes in server directory:\n".trim(shell_exec("git reset --hard HEAD 2>&1")));
-echo("\n\nPulling changes from github:\n".trim(shell_exec("git pull 2>&1")));
+echo("\n\nPulling changes from github:\n".trim(shell_exec("git pull origin indexv2 2>&1")));
 echo("\n\ngit status:\n");
 echo(trim(shell_exec("git status 2>&1")));
 ?>

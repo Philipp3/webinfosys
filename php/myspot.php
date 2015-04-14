f27a68c72eb535429e6c3dd38dd27fbca8a44f01
Philipp Peringer <phirxos@gmail.com>
2015-04-14 14:37:15 +0200

    adding myspot functionality to index.php

--- php/myspot.php
+++ php/myspot.php
@@ -6,121 +6,42 @@ require_once SERVERPATH . "php/templates/template.php";
 require_once SERVERPATH . "php/database/database.php";
 require_once SERVERPATH . "php/user.php";
 
-function prepMyspotTemplate() {
+function prepMyspotTemplate($maintemplate) {
 	$db = database\Database::getInstance ()->connect ();
 	$template = null;
 	
 	$usermgr = user\userMgr::getInstance ();
 	
-	$infomsg = "";
-	
-	
-	if (isset ( $_GET ["chgspot"] )) {
-		if (isset ( $_GET ["name"] )) {
-			$name = $_GET ["name"];
-			if ($name == "") {
-				$infomsg = "!Fehler - Feld \"Name\" darf nicht leer sein.";
-			} else {
-				$stmt = $db->prepare ( "SELECT * FROM myspots WHERE name=?" );
-				$stmt->execute ( array (
-						$name 
-				) );
-				if ($stmt->rowCount () == 0) {
-					$stmt = $db->prepare ( "INSERT INTO myspots(name,location,description) VALUES(:name,:location,:description)" );
-					$stmt->execute ( array (
-							":name" => $name,
-							":location" => $_GET ["loc"],
-							":description" => $_GET ["desc"] 
-					) );
-					$infomsg = "#Spot $name hinzugefügt";
-				} else {
-					$stmt = $db->prepare ( "UPDATE myspots SET location=:location, description=:description WHERE name=:name" );
-					$stmt->execute ( array (
-							":name" => $name,
-							":location" => $_GET ["loc"],
-							":description" => $_GET ["desc"] 
-					) );
-					$infomsg = "#Spot $name geändert";
-				}
-			}
-		}
-	}
-	
-	if ($_GET ["action"] == "edit") {
-		$template = new template\Template ( "php/templates/myspot_modify.phtml" );
-		$template->menuentries = $menuentries;
-		if (! isset ( $_GET ["spotname"] )) {
-			$template->title = "Neuen Spot hinzufügen";
-			$template->spotname = "";
-			$template->spotloc = "";
-			$template->spotdesc = "";
-			$template->out ();
-		} else {
-			$template->title = "Spot " . $_GET ["spotname"] . " editieren";
-			$stmt = $db->prepare ( "SELECT * FROM  myspots WHERE name=?" );
-			$stmt->execute ( array (
-					$_GET ["spotname"] 
-			) );
-			if ($stmt->rowCount () == 0) {
-				$template->title = "Spot " . $_GET ["spotname"] . " hinzufügen";
-				$template->spotname = $_GET ["spotname"];
-				$template->spotloc = "";
-				$template->spotdesc = "";
-			} else {
-				$data = $stmt->fetch ();
-				$template->spotname = $_GET ["spotname"];
-				$template->spotloc = $data ["location"];
-				$template->spotdesc = $data ["description"];
-			}
-		}
+	if(isset($_GET["searchSpotName"])) {
+		//display results from a search after a certain spotname
+	} else if(isset($_GET["spotname"])) {
+		//Handle viewing, editing, deleting etc. of chosen spot
 	} else {
-		$template = new template\Template ( "php/templates/myspot_main.phtml" );
-		$template->menuentries = $menuentries;
-		if (! isset ( $_GET ["search"] )) {
-			$template->title = "Myspots Startseite";
-			$data = $db->query ( "SELECT * FROM myspots" );
-			$template->groupcount = ceil ( count ( $data ) / 10 );
-			$start = 0;
-			if (isset ( $_GET ["start"] ))
-				$start = $_GET ["start"];
-			$template->currentpage = $start;
-			$template->data = array_slice ( $data, 10 * ($start - 1), 10 );
-			if ($_GET ["action"] == "delete") {
-				$delete = $_GET ["spotname"];
-				$stmt = $db->prepare ( "SELECT * FROM  myspots WHERE name=?" );
-				$stmt->execute ( array (
-						$delete 
-				) );
-				if ($stmt->rowCount () > 0) {
-					$stmt = $db->prepare ( "DELETE FROM myspots WHERE name=?" );
-					$stmt->execute ( array (
-							$delete 
-					) );
-					$infomsg = "#Spot $delete gelöscht.";
-				} else
-					$infomsg = "!Löschen gescheitert - Spot $delete nicht gefunden.";
-			}
-		} else {
-			$template->title = "Suchergebnisse für " . $_GET ["search"];
-			$stmt = $db->prepare ( "SELECT * FROM  myspots WHERE name LIKE ?" );
-			$template->groupcount = 1;
-			$template->currentpage = 1;
-			$stmt->execute ( array (
-					$_GET ["search"] 
-			) );
-			$template->data = $stmt->fetchAll ();
+		//Simple spot list without any other seettings
+		$pagNumber = 1;
+		if(isset($_GET["page"]) && is_int($_GET["page"]))
+			$pagNumber = $_GET["page"];
+		$template = new template\Template(SERVERPATH . "php/templates/myspot_list.phtml");
+		$template->title = "Spotliste";
+		$query = $db->query("SELECT COUNT(*) FROM spotlist");
+		$spotcount = $query->fetch()[0];
+		if(($page-1)*10 >= spotcount) {
+			$template->pageTooHigh = true;
+			$pagNumber = max(1,ceil(spotcount/10));
 		}
-		if ($infomsg != "") {
-			$template->infotype = "infomsg";
-			if (substr ( $infomsg, 0, 1 ) == "#")
-				$template->infotype = "infosucc";
-			else if (substr ( $infomsg, 0, 1 ) == "!")
-				$template->infotype = "infoerr";
-			$template->infomsg = substr ( $infomsg, 1 );
-		}
-		$template->infomsg = $infomsg;
+		$query = $db->query("SELECT name FROM myspots LIMIT ".(pagNumber-1)*10, 10);
+		if($spotcount > 10)
+			$template->showPaginator = true;
+		$template->spotCount = $spotcount;
+		$template->currPagVal = $pagNumber;
+		$template->spotNrStart = $pagNumber*10-9;
+		$template->spotNrEnd = min($pagNumber*10, $spotcount+10-$pagNumber*10);
+		$template->spotdata = $query->fetchAll();
+		$template->pagPages = ceil($spotcount/10);
 	}
+	
 	return $template;
+	
 }
 
 /*

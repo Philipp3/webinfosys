61cd8f022fb0f7919e99206e7df8a649fe922843
Andreas J. Maislinger <andreas.maislinger@student.uibk.ac.at>
2015-04-19 21:15:15 +0200

    fixed paginator for search........

--- php/myspot.php
+++ php/myspot.php
@@ -128,7 +128,7 @@ function getBasicSpotTemplate($db, $search=null) {
 
 	if ($search != null){
 		$template->title = "Suchergebnisse";
-		$template->$search = $search;
+		$template->search = $search;
 		$stmt = $db->prepare ( "SELECT COUNT(*) FROM myspots WHERE name LIKE ?" );
 		$stmt->execute(array("%{$search}%"));
 

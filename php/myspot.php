f9fb38b65361f11fa4a5e153e8a9325931f9cc15
Andreas J. Maislinger <andreas.maislinger@student.uibk.ac.at>
2015-04-19 15:27:07 +0200

    edit, delete

--- php/myspot.php
+++ php/myspot.php
@@ -7,77 +7,119 @@ const SERVERPATH = \grp12\SERVERPATH;
 require_once SERVERPATH . "php/templates/template.php";
 require_once SERVERPATH . "php/database/database.php";
 require_once SERVERPATH . "php/user.php";
+
+
 function prepMyspotTemplate() {
+
 	$db = \grp12\database\Database::getInstance ()->connect ();
 	$template = null;
 	
 	$usermgr = \grp12\user\userMgr::getInstance ();
-	
-	if (isset ( $_GET ["search"] )) {
+
+	if (isset ( $_POST["spotname"] ) { //add or modify
+
+		if (! $usermgr->loggedin) {
+			header('Location: /login/');
+			die();
+		}
+
+		$spotname = $_POST["spotname"];
+		$template = getBasicSpotTemplate ($db);
+
+		if ($spotname == ""){
+			$template->infomsg = "Name darf nicht leer sein.";
+			return $template;
+		}
+
+		$stmt = $db->prepare("SELECT * FROM myspots WHERE name=?");
+		$stmt->execute(array($spotname));
+
+		if ($stmt->rowCount () > 0) { //edit
+			$stmt = $db -> prepare("UPDATE myspots SET location=:location, description=:description WHERE name=:name");
+			$stmt -> execute(array(":name" => $spotname, ":location" => $_POST["loc"], ":description" => $_POST["desc"]));
+			$infomsg = "#Spot $spotname geändert";
+		} else { //add
+			$stmt = $db -> prepare("INSERT INTO myspots(name,location,description) VALUES(:name,:location,:description)");
+			$stmt -> execute(array(":name" => $spotname, ":location" => $_POST["loc"], ":description" => $_POST["desc"]));
+			$infomsg = "#Spot $spotname hinzugefügt";
+		}
+
+	} else if (isset ( $_GET ["search"] )) {  //search
+
 		$search = $_GET ["search"];
 		$template = getBasicSpotTemplate ($db,$search);
 
-	} else if (isset ( $_GET ["spotname"] )) {
-		// Handle viewing, editing, deleting etc. of chosen spot
+	} else if (isset($_GET["add"])){
+
+		$template = new \grp12\template\Template ( SERVERPATH . "php/templates/myspot_modify.phtml" );
+		$template->header = "Spot hinzufügen";
+
+	} else if (isset ( $_GET ["spotname"] )) { //single spot, display or action
 
 		$spotname = $_GET["spotname"];
 		$stmt = $db->prepare("SELECT * FROM myspots WHERE name=?");
 		$stmt->execute(array($spotname));
 
+		if ($stmt->rowCount () <= 0) {
+			$template = getBasicSpotTemplate ($db);
+			$template->infomsg = "Spot " . $spotname . " existiert nicht.";
+			return $template;
+		}
+
 		if (isset ( $_GET ["action"] )) {
 			if (! $usermgr->loggedin) {
-				//TODO redirect login
+				$template = getBasicSpotTemplate ($db);
+				$template->infomsg = "Bitte loggen sie sich ein.";
+				return $template;
 			}
 
 			if ($_GET ["action"] == "delete") {
+
+				//TODO delete image files
+
+				$stmt = $db->prepare("DELETE FROM myspots, myspots_images WHERE name=?");
+				$stmt->execute(array($spotname));
+
 				$template = getBasicSpotTemplate ($db);
-
-				if ($stmt->rowCount () > 0) {
-
-					//TODO delete image files
-
-					$stmt = $db->prepare("DELETE FROM myspots, myspots_images WHERE name=?");
-					$stmt->execute(array($spotname));
-
-					$template->infomsg = "Spot " . $spotname . " wurde gelöscht.";
-				} else {
-					$template->infomsg = "Spot " . $spotname . " existiert nicht.";
-				}
+				$template->infomsg = "Spot " . $spotname . " wurde gelöscht.";
 
 			} else if ($_GET ["action"] == "edit") {
-				//$template = 
-			} else if ($_GET ["action"] == "new") {
-				// TODO implement function for adding new spot
+
+				$template = new \grp12\template\Template ( SERVERPATH . "php/templates/myspot_modify.phtml" );
+
+				$template->defaults = $stmt->fetch();
+				$template->header = $spotname . " ändern";
+				
+			} else {
+
+				$template = getBasicSpotTemplate ($db);
+				$template->infomsg = "Ungültige Aktion.";
 			}
 
 		} else { //display details & images
 
-			if ($stmt->rowCount() > 0) {
-				$template = new \grp12\template\Template ( SERVERPATH . "php/templates/myspot_detail.phtml" );
-				$row = $stmt->fetch();
-				$template->spotname = $row ["name"];
-				$template->spotloc = $row ["location"];
-				$template->spotdesc = $row ["description"];
-				$template->loggedin = $usermgr->loggedin;
+			$template = new \grp12\template\Template ( SERVERPATH . "php/templates/myspot_detail.phtml" );
+			$row = $stmt->fetch();
+			$template->spotname = $row ["name"];
+			$template->spotloc = $row ["location"];
+			$template->spotdesc = $row ["description"];
+			$template->loggedin = $usermgr->loggedin;
 
-				$stmt_i = $db->prepare("SELECT * FROM myspots_images WHERE name=?");
-				$stmt_i->execute (array($spotname));
-				if ($stmt_i->rowCount() > 0){
-					$template->images = $stmt_i->fetchAll();
-				}
-
-			} else {
-				$template = getBasicSpotTemplate ($db);
-				$template->infomsg = "Spot " . $spotname . " existiert nicht.";
+			$stmt_i = $db->prepare("SELECT * FROM myspots_images WHERE name=?");
+			$stmt_i->execute (array($spotname));
+			if ($stmt_i->rowCount() > 0){
+				$template->images = $stmt_i->fetchAll();
 			}
+
 		}
 	} else {
-		// Simple spot list without any other seettings
+		// Simple spot list without any other settings
 		$template = getBasicSpotTemplate ($db);
 	}
 	
 	return $template;
 }
+
 function getBasicSpotTemplate($db, $search=null) {
 	$pagNumber = 1;
 	if (isset ( $_GET ["page"] ) && is_numeric ( $_GET ["page"] ))
